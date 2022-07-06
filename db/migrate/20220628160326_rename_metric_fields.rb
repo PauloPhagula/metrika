7d2338534d99dbeadfbd00e5b88f3317dafccab6
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2022-07-06 16:23:51 +0200

    fix: Correct migration name
    Somehow it got the wrong name in previous refactor

--- db/migrate/20220628160326_rename_metric_fields.rb
+++ db/migrate/20220628160326_rename_metric_fields.rb
@@ -1,4 +1,4 @@
-class IndexTimePoints < ActiveRecord::Migration[7.0]
+class RenameMetricFields < ActiveRecord::Migration[7.0]
   def change
     rename_column :metrics, :time, :timepoint
     rename_column :metrics, :value, :metric_value
