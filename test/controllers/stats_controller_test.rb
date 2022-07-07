35650a313f942d254f9acec4a02410c00c1f9402
Paulo Phagula <PauloPhagula@users.noreply.github.com>
2022-07-07 07:59:52 +0200

    test: Add validations test cases

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -1,6 +1,20 @@
 require "test_helper"
 
 class StatsControllerTest < ActionDispatch::IntegrationTest
+  test "should reject getting stats with invalid avg filter" do
+    get api_v1_stats_url,
+      params: {metric: "all", by: "eon", from: DateTime.now, to: DateTime.now.beginning_of_day},
+      as: :json
+    assert_response :bad_request
+  end
+
+  test "should reject getting stats with invalid time filter" do
+    get api_v1_stats_url,
+      params: {metric: "all", by: "minute", from: DateTime.now, to: DateTime.now.beginning_of_day},
+      as: :json
+    assert_response :bad_request
+  end
+
   test "should get stats for all metrics" do
     get api_v1_stats_url,
       params: {metric: "all", by: "minute", from: DateTime.now.beginning_of_day, to: DateTime.now},
